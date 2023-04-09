b1d111c9466a05f2df6d4f909a5c10c7b899dfe7
bsj_m.matsumoto <m.matsumoto@bravesoft.co.jp>
2023-04-09 16:55:09 +0900

    pagesに作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
+<script lang="ts" setup>
+const task = ref<String>()
+const taskGroup = ref<Array>([])
+
+function addTaskGroup() {
+  taskGroup.value.push(task.value)
+  task.value = ''
+}
+</script>
+
 <template>
   <v-app>
-    <AppSample />
+    <v-app-bar :title="'✔️Nuxt3-TODO'"></v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-text-field v-model="task" clearable :label="'タスクを入力'"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn @click="addTaskGroup">追加</v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <p>{{ taskGroup }}</p>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
